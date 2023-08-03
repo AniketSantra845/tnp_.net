bd8920cd0699d23ff665923acd25a9334234a8c3
Aniket <Aniket@DESKTOP-03HPECQ>
2023-08-03 22:33:26 +0530

    nothing

--- Demo/api/Webapi.cs
+++ Demo/api/Webapi.cs
@@ -7,8 +7,8 @@ namespace Demo.api
         HttpClient client;
         HttpResponseMessage response;
         //Uri baseAddress = new Uri("http://127.0.0.1//api/api_modify.php?what=");
-        Uri baseAddress = new Uri("https://tnpdeveloper.000webhostapp.com/api/api_modify.php?what=");
-        //Uri baseAddress = new Uri("https://tnp.srlimba.ac.in/srl/api_modify.php?what=");
+        //Uri baseAddress = new Uri("https://tnpdeveloper.000webhostapp.com/api/api_modify.php?what=");
+        Uri baseAddress = new Uri("https://tnp.srlimba.ac.in/srl/api_modify.php?what=");
 
         public System.Uri api()
         {   
